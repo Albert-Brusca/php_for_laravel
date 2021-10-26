741739d78f13f827afdb0b17fa0afb65dafdbd87
Albert-Brusca <abrusca@iesebre.com>
2021-10-26 15:50:50 +0200

    Neteja codi Task.php

--- php_for_laravel_developers/app/index.php
+++ php_for_laravel_developers/app/index.php
@@ -4,16 +4,26 @@ require 'app/helpers.php';
 
 require 'app/Task.php';
 
-//$task = new Task(1,'comprar pa', 'a la panaderia', 0);
-//var_dump($task);
+require 'config.php';
 
-// API (Aplication Public Interface)
+//$user = 'debian-sys-maint';
+//$pass = 'vNlMOKJcn8fINrQ8';
+//$dsn = 'mysql:host=localhost;dbname=phplaraveldevs';
 
-$user = 'debian-sys-maint';
-$pass = 'vNlMOKJcn8fINrQ8';
+
+
+$user = $config['database']['user'];
+$pass = $config['database']['password'];
+$type = $config['database']['databasetype'];
+$host = $config['database']['host'];
+$name = $config['database']['name'];
+$dsn = "$type:host=$host;dbname=$name";
+
+//connect -> API nom connect, sense parametres entrada i tornara objecte $dbh
+
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs', $user, $pass);
+    $dbh = new PDO($dsn, $user, $pass);
 
 } catch (\Exception $e) {
     echo 'Error de connexió a la base de dades';
@@ -25,7 +35,4 @@ $statement->execute();
 
 $tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 
-
-
 $greeting = greet();
-//$greeting = 'Hola ' . $_GET['name'] . ' ' . $_GET['name'] . '!';
---
+++ php_for_laravel_developers/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'database' => [
+        'user' => 'debian-sys-maint',
+        'password' => 'vNlMOKJcn8fINrQ8',
+        'databasetype' => 'mysql',
+        'host' => 'localhost',
+        'name' => 'phplaraveldevs',
+    ]
+];
